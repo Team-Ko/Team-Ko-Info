88f0cd1b7d00a8941de92c882ba9ed9f31b4097f
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-06-03 16:38:07 +0900

    Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import HeaderImg from "../../asset/bg.jpg";
+import Typist from "react-typist";
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 600px;
+  background-image: url(${HeaderImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const Blank = styled.div`
+  height: ${(props) => props.h}px;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => (props.size ? props.size : 63)}px;
+  color: white;
+  text-align: center;
+  margin: 0;
+`;
+
+const SubTitle = styled.h3`
+  font-size: ${(props) => (props.size ? props.size : 24)}px;
+  color: white;
+  text-align: center;
+  margin: 0;
+  font-weight: 600;
+`;
+
+const TitleBox = styled.div`
+  background-color: rgba(0, 0, 0, 0.2);
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Blank h={200}></Blank>
+      <Typist>
+        <TitleBox>
+          <Title>Team Ko</Title>
+          <Typist.Delay ms={500} />
+          <Blank h={10}></Blank>
+          <SubTitle>Make What We Want</SubTitle>
+        </TitleBox>
+      </Typist>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
