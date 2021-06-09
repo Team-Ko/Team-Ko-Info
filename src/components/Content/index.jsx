b6e30268e7578bbda16b2af0985870eb1ba9728e
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-06-09 10:27:03 +0900

    개발환경 소개

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -2,35 +2,59 @@ import React from "react";
 import styled from "styled-components";
 
 const InnerContainer = styled.div`
-    width: 100vw;
+    width: 100%;
     height: 1000px;
     background-color: black;
 `;
 
 const Title = styled.div`
   color: white;
-  font-size: 42px;
+  font-size: ${(props) => props.size}px;
   font-weight: bold;
   text-align: center;
-  border-bottom: 1px solid rgb(127, 127, 127);
-  width: 600px;
   padding-bottom: 20px;
   margin: 0 auto;
 `;
 
-const SubTitle = styled.div`
+const Sub = styled.div`
   color: white;
-  font-size: 32px;
+  font-size: ${(props) => props.size}px;
   text-align: center;
   margin: 0 auto;
   padding-top: 10px;
 `;
 
+const UnderBar = styled.div`
+  border-top: 1px solid rgb(127, 127, 127);
+  width: 600px;
+  margin: 0 auto;
+`;
+
+const Introduction = styled.div`
+  color: white;
+  font-size: ${(props) => props.size}px;
+  margin: 0 auto;
+  padding-top: 10px;
+  width : 650px;
+`;
+
+const Blank = styled.div`
+  height: ${(props) => props.h}px;
+`;
+
 const Content = () => {
   return (
   <InnerContainer>
-    <Title>Team-Ko를 소개합니다</Title>
-    <SubTitle>우리가 하는 일들</SubTitle>
+    <Title size = {42}>Team-Ko를 소개합니다</Title>
+    <UnderBar />
+    <Sub size = {32}>우리가 하는 일들</Sub>
+    <Blank h={300} />
+    <Title size = {48}>우리의 개발환경은 이렇습니다</Title>
+    <Introduction size = {24}>
+      <li>소스는 내부 GitLab 저장소를 사용합니다.</li>
+      <li>프로젝트 관리는 Notion을 통해 관리합니다.</li>
+      <li>같은 내용을 정할 것</li>
+    </Introduction>
   </InnerContainer>
   );
 };
